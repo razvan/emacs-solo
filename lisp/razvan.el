e473e9700db0e72264051c736fe8195c94784b24
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2026-07-27 11:20:46 +0200

    fix: disable more tab characters

--- lisp/razvan.el
+++ lisp/razvan.el
@@ -1,3 +1,7 @@
-;; Disable TABs
+;;; package -- Disable TABs
 (add-hook 'prog-mode-hook #'emacs-solo/prefer-spaces)
 (add-hook 'text-mode-hook #'emacs-solo/prefer-spaces)
+(setq-default indent-tabs-mode nil)
+
+(provide 'razvan)
+;;; razvan.el ends here
