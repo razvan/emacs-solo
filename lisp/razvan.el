6c417b855a49520a45b8ed3d5f4934072528464c
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2026-07-27 17:10:22 +0200

    small tweaks

--- lisp/razvan.el
+++ lisp/razvan.el
@@ -3,5 +3,15 @@
 (add-hook 'text-mode-hook #'emacs-solo/prefer-spaces)
 (setq-default indent-tabs-mode nil)
 
+;; Ensure this keybinding works without having to cal magit-status manually once.
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit-status))
+
+;;; Place cursor at the end of the scratch buffer at startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (with-current-buffer "*scratch*"
+              (goto-char (point-max)))))
 (provide 'razvan)
 ;;; razvan.el ends here
